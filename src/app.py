c35ddea6e979419bfcc1e55d69ed0ba8d9d5e181
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-12 10:17:33 +0100

    * Add rendering of menu

--- src/app.py
+++ src/app.py
@@ -1,45 +1,28 @@
 from game_engine.fltk import *
 from typing import Literal
+from view.MenuView.menu_view import MenuView
 
 def main():
     state: Literal["MENU", "GAME", "GAME_OVER"] = "MENU"
-    width, height = 900, 700
+    width, height = 650, 650
+    menu = MenuView()
     
-    cree_fenetre(width, height, redimension=True)
+    cree_fenetre(width, height)
+    menu.render()
+
     while True:
-        event, event_type = donne_ev(), type_ev(donne_ev())
-        efface_tout()
+        event = donne_ev()
+        event_type = type_ev(event)
+        choice = menu.handle_event(event, event_type)
 
-        if state == "MENU":
-            texte((width / 2) - 130, 60, "WALL IS YOU — MENU")
-            texte((width / 2) - 150, 100, "[Enter] to play, [Q] to quit")
-
-            if event_type == "Touche":
-                match touche(event):
-                    case "Return": state = "GAME"
-                    case 'q' | 'Q' | "Escape": break
-        
-        elif state == "GAME":
-            texte((width / 2) - 130, 650, "Game: [Space] movement, [R] reset, [Esc] menu")
-
-            if event_type == "Touche":
-                match touche(event):
-                    case "Escape": state = "MENU"
-                    # case "r": 
-                    # case "space":
-
-        elif state == "GAME_OVER":
-            texte((width / 2) - 130, 60, "Game Over")
-            texte((width / 2) - 150, 100, "[Enter] to play again, [Esc] back to menu")
-            
-            if event_type == "Touche": 
-                match touche(event):
-                    case "Escape": state = "MENU"
+        if choice:
+            if choice["action"] == "start":
+                print("Selected level:", choice["level"])
+                print("Options:", choice["options"])
+            elif choice["action"] == "quit":
+                break
 
         mise_a_jour()
-            
-        attend_clic_gauche()
-        ferme_fenetre()
     
 if __name__ == '__main__':
     main()
