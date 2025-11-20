3d5513e76fb2bfee915c1190ff4b1cf8e0f6fb44
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-20 19:40:15 +0100

    * Replace load_levels from model/ to src/
    - load_loaders - parser of levels in json files

---
+++ src/load_levels.py
@@ -0,0 +1,40 @@
+from typing import Tuple, List, Dict
+from pathlib import Path
+from .model.dungeon import Dungeon
+from .model.cell import Cell
+import json
+
+GridSrc = List[List[dict]]
+Grid = List[List[Cell]]
+
+def load_levels(path: str | Path) -> Tuple[Dungeon, Dict, List]:
+    data = json.loads(Path(path).read_text(encoding="utf-8"))
+    grid_src: GridSrc = data["grid"]
+    grid: Grid = []
+    
+    for row in grid_src:
+        grid.append([
+            Cell(
+                north = el["top"],
+                east  = el["right"],
+                south = el["bottom"],
+                west  = el["left"]
+            ) for el in row
+        ])
+    
+    dungeon = Dungeon(grid)
+    hero_data = data["hero"]
+    hero = {
+        "level": hero_data["level"],
+        "position": [hero_data["row"], hero_data["col"]]
+    }
+    dragons = [
+        {
+            "level": el["level"],
+            "position": [el["row"], el["col"]]
+        }
+        for el in data["dragons"]
+    ]
+    
+    return dungeon, hero, dragons
+    
