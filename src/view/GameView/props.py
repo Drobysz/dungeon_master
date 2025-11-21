1bddc5c578f36bcb46800a58f04d3237a40bbf05
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-21 03:08:30 +0100

    * Add color properties for GameView

---
+++ src/view/GameView/props.py
@@ -0,0 +1,13 @@
+from .classes import Color
+
+BACKGROUND: Color = "#0e0f13"
+GRID_BG: Color = "#14161d"
+GRID_BORDER: Color = "#242936"
+CELL_BORDER: Color = "#3a4050"
+CELL_DOOR: Color = "#8fd0ff"
+HERO_COLOR: Color = "#7ee081"
+DRAGON_COLOR: Color = "#ff7575"
+PATH_COLOR: Color = "#ff5050"
+TEXT_COLOR: Color = "#e6e6e6"
+MUTED_COLOR: Color = "#8a8f98"
+OVERLAY_BG: Color = "black"
