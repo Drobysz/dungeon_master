a09d7a44bf6bacb9f3d86c5fd5cc32aa92262cb8
Drobysz <alexanderdrobyschevski@gmail.com>
2025-12-07 17:42:09 +0100

    * Add 2 helper functions * Add special symbols dictionary
    Functions)
    
    get_content_from_file(path):
    
      - It accepts as parameter an abolute path to the file with
        desired content extracts and returns it.
    
    convert_map(path):
    
      - It converts special symbols extracted from text file
        into matrix with objects of cell states.
    
    symbol dictionary)
    
      - It represents a library of special sybols meanings.
    
      - The dictionary is indexed with the syblols.
    
      - Values consist of states of cell walls.

--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,43 @@
 from pathlib import Path
-from typing import List
+from typing import List, Dict
+
+_THIS = Path(__file__).resolve()
+_PROJECT_ROOT = _THIS.parents[1]
+
+Map = List[List[Dict[str, bool]]]
+
+symbols = {
+    '╬': { "top": True, "right": True, "bottom": True, "left": True},
+    
+    '╠': { "top": True, "right": True, "bottom": True, "left": False},
+    '╣': { "top": True, "right": False, "bottom": True, "left": True},
+    '╦': { "top": False, "right": True, "bottom": True, "left": True},
+    '╩': { "top": True, "right": True, "bottom": False, "left": True},
+    
+    '═': { "top": False, "right": True, "bottom": False, "left": True},
+    '║': { "top": True, "right": False, "bottom": True, "left": False},
+    '╔': { "top": False, "right": True, "bottom": True, "left": False},
+    '╗': { "top": False, "right": False, "bottom": True, "left": True},
+    '╚': { "top": True, "right": True, "bottom": False, "left": False},
+    '╝': { "top": True, "right": False, "bottom": False, "left": True},
+    
+    '╥': { "top": False, "right": False, "bottom": True, "left": False},
+    '╨': { "top": True, "right": False, "bottom": False, "left": False},
+    '╡': { "top": False, "right": False, "bottom": False, "left": True},
+    '╞': { "top": False, "right": True, "bottom": False, "left": False}
+}
+
 
 def get_filename(p: Path) -> str:
     return p.name.split('.')[0]
 
 
-_THIS = Path(__file__).resolve()
-_PROJECT_ROOT = _THIS.parents[1]
-
 def get_complete_path(PATH):
     return _PROJECT_ROOT / PATH
 
+
 def list_files(path = "levels", ext = "json") -> List[Path]:
-    LEVELS_DIR = _PROJECT_ROOT / path
+    LEVELS_DIR = get_complete_path(path)
 
     if not LEVELS_DIR.exists():
         return []
@@ -20,3 +45,32 @@ def list_files(path = "levels", ext = "json") -> List[Path]:
              	if p.is_file() and p.suffix.lower() == "." + ext]
     files.sort(key=lambda p: p.name.lower())
     return files
+
+
+def get_content_from_file(path: Path):
+    f = open(path, 'r')
+    content = f.read()
+    f.close()
+    
+    return content
+
+
+def convert_map(path: Path) -> Map:
+    map: Map = []
+    content = get_content_from_file(path)
+    ln = len(content)
+    i = 0
+    
+    while i < ln:
+        row = []
+        
+        while i < ln and content[i] != '\n':
+            cell = symbols[content[i]]
+            row.append(cell)
+            i += 1
+
+        map.append(row)
+        i += 1
+    
+    return map
+    
